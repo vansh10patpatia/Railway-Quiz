f7c860b356cba3322868d8121fc21f92bf2d9aea
raashi77 <rashikhatri0013@gmail.com>
2021-02-26 19:31:02 +0530

    dashboard complete

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -25,7 +25,7 @@
                with font-awesome or any other icon font library -->
          
           <li class="nav-item">
-            <a href="#" class="nav-link">
+            <a href="dashboard" class="nav-link">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
                 Dashboard
